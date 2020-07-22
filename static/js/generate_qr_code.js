611347af0a680f1c167972ed18289fcc148ca0bf
Nathaniel Tagg <nathaniel.tagg@gmail.com>
2020-07-22 16:09:23 -0400

    Change how QR codes are printed.

--- static/js/generate_qr_code.js
+++ static/js/generate_qr_code.js
@@ -1,77 +1,126 @@
+$(function(){
+  DrawQRCodes();
+  $('#qr-error-safe').on("change",DrawQRCodes);
+  ResizeQrText();
+  $( window ).resize(ResizeQrText);
+})
+ 
 
-var gqr = null;
+function ResizeQrText() {
+    $('div.qr').each(function(){
+      $(this).css('font-size',15*$(this).width()/500);
+     })
+}
+// This function to redraw all QR canvases.
+function DrawQRCodes(){
+  // lowres = false means lots of error correction, 
+  // lowres = true means low-res, easier to see
+  var lowres = ! $('#qr-error-safe').is(":checked");
+  canvases = $("canvas.qr-code");
 
-// lowres = false means lots of error correction, 
-// lowres = true means low-res, easier to see
-function DrawQRCode(canvases,text,desc,lowres){
+  // newer version: no text in canvas, use CSS to do that instead.
   console.log("DrawQRCode",...arguments);
-	$(canvases).each(function(){
-			var canvas = this;
-			var segs = qrcodegen.QrSegment.makeSegments(text);
+  $(canvases).each(function(){
+      var canvas = this;
+      var text = $(canvas).data('qr-text');
+      var desc = $(canvas).data('qr-desc');
+      console.log("Drawing QR code",text,desc,lowres);
 
-			var ecl = qrcodegen.QrCode.Ecc.HIGH;
+      var segs = qrcodegen.QrSegment.makeSegments(text);
+
+      var ecl = qrcodegen.QrCode.Ecc.HIGH;
       var minVersion = 8; // 8; // Determines size, but adds correction bits.
-
      if(lowres){
         ecl = qrcodegen.QrCode.Ecc.LOW;
-	  		minVersion = 1; // 8; // Determines size, but adds correction bits.
+        minVersion = 1; // 8; // Determines size, but adds correction bits.
       }
-			var qr = qrcodegen.QrCode.encodeSegments(segs, ecl, minVersion);
+      var qr = qrcodegen.QrCode.encodeSegments(segs, ecl, minVersion);
       gqr = qr;
-	    console.log(qr);
+      console.log(qr);
       var w = canvas.width;
-      var scale = 12;//Math.Round(w/12*8);
-      var border = 8; //w/12*4;
+      var scale = 8;//Math.Round(w/12*8);
+      var border = 0; //w/12*4;
 
-  	// 	var scale = 8; // pixels per module
-			// var border = 8;//4; // modules
-			// var svg = document.getElementById("qrcode-svg");
-			// canvas.style.display = "none";
-			// svg.style.display = "none";
+      var ctx = canvas.getContext("2d");
+      qr.drawCanvas(scale, border, canvas);
+  })
 
-			var ctx = canvas.getContext("2d");
-			qr.drawCanvas(scale, border, canvas);
-			ctx.font = "16px Inconsolata";
-			ctx.fillStyle = "black";
-			var width = canvas.width;
-			var height = canvas.height;
-			ctx.save();
-			ctx.translate(width/2,height/2);
 
-			ctx.save();
-			ctx.translate(-width/2,-height/2);
+ //  // This is the older version that puts in the text in the canvas.
+ //  console.log("DrawQRCode",...arguments);
+	// $(canvases).each(function(){
+	// 		var canvas = this;
+ //      var text = $(canvas).data('qr-text');
+ //      var desc = $(canvas).data('qr-desc');
 
-        ctx.scale(width/520,width/520);
-  			ctx.fillText(text,14,14);
-  			if(desc) ctx.fillText(desc,14,31);
+	// 		var segs = qrcodegen.QrSegment.makeSegments(text);
 
-			ctx.restore();
+	// 		var ecl = qrcodegen.QrCode.Ecc.HIGH;
+ //      var minVersion = 8; // 8; // Determines size, but adds correction bits.
 
-			ctx.rotate(90*Math.PI/180.)
-			ctx.save();
-			ctx.translate(-width/2,-height/2);
-        ctx.scale(width/520,width/520);
-        ctx.fillText(text,14,14);
-        if(desc) ctx.fillText(desc,14,31);
-			ctx.restore();
+ //      console.log("Drawing QR code",text,desc,lowres);
 
-			ctx.rotate(90*Math.PI/180.)
-			ctx.save();
-			ctx.translate(-width/2,-height/2);
-        ctx.scale(width/520,width/520);
-        ctx.fillText(text,14,14);
-        if(desc) ctx.fillText(desc,14,31);
-			ctx.restore();
+ //     if(lowres){
+ //        ecl = qrcodegen.QrCode.Ecc.LOW;
+	//   		minVersion = 1; // 8; // Determines size, but adds correction bits.
+ //      }
+	// 		var qr = qrcodegen.QrCode.encodeSegments(segs, ecl, minVersion);
+ //      gqr = qr;
+	//     console.log(qr);
+ //      var w = canvas.width;
+ //      var scale = 12;//Math.Round(w/12*8);
+ //      var border = 8; //w/12*4;
 
-			ctx.rotate(90*Math.PI/180.)
-			ctx.save();
-			ctx.translate(-width/2,-height/2);
-        ctx.scale(width/520,width/520);
-        ctx.fillText(text,14,14);
-        if(desc) ctx.fillText(desc,14,31);
-			ctx.restore();
+ //  	// 	var scale = 8; // pixels per module
+	// 		// var border = 8;//4; // modules
+	// 		// var svg = document.getElementById("qrcode-svg");
+	// 		// canvas.style.display = "none";
+	// 		// svg.style.display = "none";
 
-			ctx.restore();
-	})
+	// 		var ctx = canvas.getContext("2d");
+	// 		qr.drawCanvas(scale, border, canvas);
+	// 		ctx.font = "16px Inconsolata";
+	// 		ctx.fillStyle = "black";
+	// 		var width = canvas.width;
+	// 		var height = canvas.height;
+	// 		ctx.save();
+	// 		ctx.translate(width/2,height/2);
+
+	// 		ctx.save();
+	// 		ctx.translate(-width/2,-height/2);
+
+ //        ctx.scale(width/520,width/520);
+ //  			ctx.fillText(text,14,14);
+ //  			if(desc) ctx.fillText(desc,14,31);
+
+	// 		ctx.restore();
+
+	// 		ctx.rotate(90*Math.PI/180.)
+	// 		ctx.save();
+	// 		ctx.translate(-width/2,-height/2);
+ //        ctx.scale(width/520,width/520);
+ //        ctx.fillText(text,14,14);
+ //        if(desc) ctx.fillText(desc,14,31);
+	// 		ctx.restore();
+
+	// 		ctx.rotate(90*Math.PI/180.)
+	// 		ctx.save();
+	// 		ctx.translate(-width/2,-height/2);
+ //        ctx.scale(width/520,width/520);
+ //        ctx.fillText(text,14,14);
+ //        if(desc) ctx.fillText(desc,14,31);
+	// 		ctx.restore();
+
+	// 		ctx.rotate(90*Math.PI/180.)
+	// 		ctx.save();
+	// 		ctx.translate(-width/2,-height/2);
+ //        ctx.scale(width/520,width/520);
+ //        ctx.fillText(text,14,14);
+ //        if(desc) ctx.fillText(desc,14,31);
+	// 		ctx.restore();
+
+	// 		ctx.restore();
+	// })
 
 };
+
