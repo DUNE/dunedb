23cf9856d9b3672d9e21889df947bc346a180194
Nathaniel Tagg <ntagg@otterbein.edu>
2020-07-23 18:49:38 +0000

    work on server.

--- pm2.config.server.js
+++ pm2.config.server.js
@@ -1,9 +1,10 @@
 module.exports = {
+  // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
   apps : [{
     name: 'sietch',
     script: 'index.js',
+    cwd: '/home/tagg/Sietch',
 
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     args: ['--color'],
     instances: 1,
     autorestart: true,
@@ -18,6 +19,25 @@ module.exports = {
     combine_logs: true,
 
   }],
+  [{
+      name: 'sietch-dev',
+      script: 'index.js',
+      cwd: '/home/tagg/dev',
+
+      args: ['--color'],
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      },
+      combine_logs: true,
+
+    }],
 
   // deploy : {
   //   production : {
