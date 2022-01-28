5369e0e24706003483828372b55d43aee4363dd8
Krishanu Majumdar <majumdar@liverpool.ac.uk>
2022-01-28 09:59:20 +0000

    Overhaul of the Users related code: - removed the ability to edit user information in the DB interface ... this was a major security flaw, in that any user could in fact edit any other user's information - not just their own!  And these sorts of changes should be done through Auth0 anyway, not the DB - removed the ability to promote users through the DB interface ... changes to user roles and permissions should be done centrally through Auth0 from now on so that we don't end up with the mess of roles and permissions that we currently have - moved the current user's roles and permissions information to the Profile page ... reduces the amount of clutter on the homepage - cleaned up and made formatting and layout consistent on the user and m2m user list pages

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,211 +1,72 @@
+
 'use strict';
 
-// General pug requirements
-const chalk = require('chalk');
 const express = require('express');
+const m2m = require("lib/m2m.js");
 var ManagementClient = require('auth0').ManagementClient;
-
-// Local Javascript libraries
-const email = require("lib/email.js");
 var permissions = require('lib/permissions.js');
+
+/// To ensure the manager below works:
+///    - go to auth0 dashboard -> Applications -> APIs -> Auth0ManagementAPI
+///    - go to the "Machine to Machine Applications" tab
+///    - set the Sietch application to "Authorized"
+///    - using the pulldown arrow on the right, authorize the scopes given below
+/// There is no issue using the same 'clientId' and 'clientSecret' that we use for the main authentication
+var manager = new ManagementClient({domain      : config.auth0_domain,
+                                    clientId    : config.auth0_client_id,
+                                    clientSecret: config.auth0_client_secret,
+                                    scope       : 'read:users update:users read:roles'});
 
 var router = express.Router();
 module.exports = router;
 
 
-var manager = new ManagementClient(
+// List all human users
+router.get('/users', permissions.checkPermission("users:view"), async function(req, res, next)
 {
-  // To ensure this works:
-  // Go to auth0 dashboard -> Applications -> APIs -> Auth0ManagementAPI
-  // Go to the "Machine to Machine Applications" tab
-  // Turn on the Sietch application to "Authorized"
-  // Use the pulldown arrow on the right and authorize the scopes shown below
-  // There is no issue using the same authentication clientId and clientSecret that we use for the main authentication.
-
-  domain: config.auth0_domain,
-  clientId: config.auth0_client_id,
-  clientSecret: config.auth0_client_secret,
-  scope: 'read:users update:users read:roles'
-});
-
-
-/*
-(async function()
-{
-  // Test that it works on startup
-  var u = await manager.getUsersByEmail('majumdar@liverpool.ac.uk');
-  
-  if(u.length == 0)
-  {
-    console.log("No results");
-  }
-  
-  u = u[0];
-  console.log(u);
-  
-  var user_metadata = u.user_metadata || {};
-  user_metadata.start_page = "/categories/flute";
-  
-  var r = await manager.updateUserMetadata({id: u.user_id}, user_metadata);
-  console.log("Updated user metadata");
-  console.log(r);
-})();
-*/
-
-
-// First step of user self-promotion (up to the user submitting their request)
-router.get('/user/promote', permissions.checkPermission("users:view"), function(req, res, next)
-{
-  req.session.promotion_attempts 
-  
-  // Render the self-promotion page
-  res.render("user_promote.pug");
-});
-
-// Second step of the self-promotion (after a user submits their request)
-// Note that a user can only promote themself if they're actually logged in
-router.post('/user/promote', permissions.ensureAuthenticated, async function(req, res, next)
-{
-  // Limit the retry rate
-//  logger.info(req.session.self_promotion_tries.join(','));
-
-  req.session.self_promotion_tries =  req.session.self_promotion_tries || [];
-  
-  var now = Date.now();
-  req.session.self_promotion_tries.push(now);
-  
-  var t_timeout = now -  10 * 60 * 1000;    // Use a 10 minute timeout
-  
-  // Only keep track of attempts that occurred within the last 10 minutes
-  req.session.self_promotion_tries = req.session.self_promotion_tries.filter( (t) => t > t_timeout );
-
-  var n = req.session.self_promotion_tries.length;
-  
-  if(n > 3)
-  {
-    return res.status(403).send("Too many self-promotion attempts have been made. Please wait 10 minutes before trying again.");
-  } 
-
-//  logger.info(req.body, global.config.self_promotion);
-//  logger.info("headers", req.headers);
-  logger.info("User self-promotion attempt: ip", req.ip, "tries", n);
-//  logger.info("limiter", limiter);
-  
-  if(global.config.self_promotion
-    && global.config.self_promotion[req.body.user]
-    && global.config.self_promotion[req.body.user].password == req.body.password)
-  {
-    try
-    {
-      var result = await manager.assignRolestoUser({id: req.user.user_id}, {roles: global.config.self_promotion[req.body.user].roles});            
-        
-      // Get new user info
-      var uroles = await manager.getUserRoles({id: req.user.user_id, per_page: 100});
-//      var upermissions = await manager.getUserPermissions({id: req.user.user_id, per_page: 100});
-
-      var roles = uroles.map(i => i.name);
-//      logger.info(uroles.map(i => i.name));
-//      req.user.roles = uroles.map(i => i.name);
-
-//      logger.info(upermissions.map(i => i.permission_name));
-//      req.user.permissions = upermissions.map(i => i.permission_name);
-
-      // Tell the authorities!
-      email(
-      {
-        subject: "APA DB: " + req.user.displayName + " self-promoted",
-        text: `${req.user.displayName} (${req.user.emails[0].value}) just self-promoted using username ${req.body.user}.\n\n`
-            + `Their new roles are:\n`
-            +   roles.join("\n")
-      })
-        
-//      limiter.reset(req);
-
-      // Render the promotion success page
-      res.render("user_promoteSuccess.pug", {roles});
-    }
-    catch(err)
-    {
-      logger.info(err);
-      logger.info(err.stack);
-    }
-  }
-  else
-  {
-    var message = `Invalid user/password. You are permitted only 3 self-promotion attempts before being locked out for 10 minutes.`;
-      
-    res.render("user_promote.pug", {message});
-  }
-});
-
-
-// View a list of all users
-router.get('/users', permissions.checkPermissionJson('users:view'), async function(req, res, next)
-{
-  // Render the users list page
+  // Render the page for showing a list of all human users
   res.render("list_users.pug");
 });
 
 
-// Edit a user
-router.get('/user/edit/:user_id?', permissions.checkAuthenticated, async function(req, res, next)
+// List all 'machine-to-machine' (m2m) users
+// NOTE: this page also allows individual m2m users to be edited, so requires permission beyond simple user viewing
+router.get('/users/m2m', permissions.checkPermission("users:edit"), async function(req, res, next)
 {
-  // Render the user editing page
-  res.render("edit_user.pug", {user_id: req.params.user_id});
-});
-
-
-// View the current user's public profile
-router.get('/user/profile/:userId?', permissions.checkPermission("users:view"), async function(req, res, next)
-{
-  var user_id = req.user.user_id;
-  
-  if(req.params.userId)
-  {
-    user_id = decodeURIComponent(req.params.userId);
-  }
-
-//  logger.info("Looking up", user_id);
-//  logger.info(await manager.getUserRoles({id: user_id}))
-
-  var [userProfile, userRoles] = await Promise.all(
-  [
-    manager.getUser({id: user_id}),
-    manager.getUserRoles({id: user_id})
-  ]);
-  
-  if(Array.isArray(userProfile))
-  {
-    return res.status(400).send("More than one user matched");
-  }
-  
-  if(!userProfile)
-  {
-    return res.status(400).send("No user with that ID");
-  }
-  
-//  logger.info(user);
-
-  // Render the user profile page
-  res.render("user_profile.pug", {userProfile, userRoles});
-});
-
-
-// First step of listing the machine-to-machine (m2m) users
-const m2m = require("lib/m2m.js");
-
-router.get('/m2mUsers', permissions.checkPermission("users:view"), async function(req, res, next)
-{
+  // Retrieve a list of all m2m users
   var users = await m2m.ListMachineUsers();
   
-  // Render the list of m2m users
-  res.render("m2mUsers.pug", {users});
+  // Render the page for showing a list of all m2m users
+  res.render("list_m2mUsers.pug", {users});
 });
 
-// Second step of listing the m2m users
-router.post('/m2mUsers', permissions.checkPermission("users:edit"), async function(req, res, next)
+
+// View the currently logged in user's profile
+router.get('/user/profile', permissions.checkPermission("users:view"), async function(req, res, next)
 {
-  // Render the credentials page for a specific m2m user
-  res.render("m2mUserCredentials.pug",{users: await m2m.AddMachineUser(req.body)});
+  // Get the user ID from the current user's session information
+  var userId = req.user.user_id;
+  
+  // Get the user's information and roles via the connection to Auth0
+  var [userProfile, userRoles] = await Promise.all(
+  [
+    manager.getUser({id: userId}),
+    manager.getUserRoles({id: userId})
+  ]);
+  
+  // Throw an error if there is more than one user with the same ID
+  if(Array.isArray(userProfile))
+  {
+    return res.status(400).send("There is more than one user with ID: " + userId);
+  }
+  
+  // Throw an error if there is no user with the provided ID
+  if(!userProfile)
+  {
+    return res.status(400).send("There is no user with ID: " + userId);
+  }
+  
+  // Render the page for viewing the user profile
+  res.render("user_profile.pug", {userProfile, userRoles});
 });
 
