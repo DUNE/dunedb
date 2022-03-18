30be130af6f95e0ce7943652df119a7d564f7c2e
Micah Henning <mhenning@fnal.gov>
2022-03-18 11:52:37 -0500

    add https support

--- app/index.js
+++ app/index.js
@@ -4,27 +4,43 @@ const logger = require('./lib/logger');
 
 require('app-module-path').addPath(__dirname); // Set this as the base for all 'require' lines in future.
 
-var createApp = require('./app'); 
-var http = require('http'); // to run the server
+var createApp = require('./app');
 
-logger.info(`Starting up in mode: ${NODE_ENV}`)
+let http;
+const options = {};
+if (NODE_ENV === 'development') {
+  http = require('http');
+} else {
+  http = require('https');
+  const certificate = process.env.HTTPS_CERTIFICATE;
+  const key = process.env.HTTPS_KEY;
+
+  if (!certificate)
+    throw new Error('Certificate required in $HTTPS_CERTIFICATE environment variable in non-development environments.');
+  if (!key)
+    throw new Error('HTTPS Private Key required in $HTTPS_KEY environment variable in non-development environments.');
+
+  options.key = key;
+  options.cert = certificate;
+}
+
+logger.info(`Starting up in mode: ${NODE_ENV}`);
 
 db.open()
   .then(async function run(){
-    var app = await createApp();
-
-    var httpServer = http.createServer(app);
+    const app = await createApp();
+    const server = http.createServer(options, app);
 
     function closeGracefully() {
       // see https://www.oesmith.co.uk/2012/01/08/graceful-shutdown-node-js-express.html
       // Shut down gracefully!
       logger.info("Closing on SIGTERM");
-      httpServer.close();
+      server.close();
     }
     process.on('SIGINT', closeGracefully); 
     process.on('SIGHUP', closeGracefully); 
 
-    httpServer.on('close', async function () {
+    server.on('close', async function () {
       // this runs after the above has completed. It allows us to close the database.
       logger.info("Web server closed. Shutting down DB connection.");
       await db.close();
@@ -35,5 +51,5 @@ db.open()
       process.exit();
     });
 
-    httpServer.listen(APP_PORT, () => logger.info(`listening on port ${APP_PORT}!`))  
+    server.listen(APP_PORT, () => logger.info(`listening on port ${APP_PORT}!`))  
   });
