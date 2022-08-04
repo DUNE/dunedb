a9190b301e520cb914d371e9dc55072cb43bb413
Krishanu Majumdar <majumdar@liverpool.ac.uk>
2022-08-04 22:56:55 +0100

    Miscellaneous code cleanup ... - removed auth-no-passport.js file ... wasn't being used anywhere or by anything - cleaned up utils.js file ... removed two ununsed functions, cleaned up and correctly formatted remaining code - moved README file out of old docs sub-directory and into main directory (no point in having that sub-directory any more)

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -1,34 +1,7 @@
-"use strict";
-
-var short = require('short-uuid')(); // uses base58 by default
-
-//
-/// Short snippets
-function deCamelCase(input)
-{
-  // input: someStrange_variableNumber09
-  // output: Some Strange Variable Number 09
-  var arr = input.replace(/(\d+)/,' $1')
-                 .split(/(?=[A-Z])|[ _]/);
-
-  var arr2 =[];
-  for(var w of arr) arr2.push(w.charAt(0).toUpperCase()+w.slice(1));
-  return arr2.join(' ');
-}
-
-function unshortenUuid(shortend)
-{
-  // It might be padded. Remove any trailing '-' used to pad to 22 chars.
-  var txt = shortend.match('[^\-]*')[0];
-  return short.toUUID(txt)
-}
-
 module.exports = {
- uuid_regex: ':uuid([A-Fa-f0-9]{8}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{12})',
+  // A 36 character long full UUID, of the form: [8]-[4]-[4]-[4]-[12]
+  uuid_regex: ':uuid([A-Fa-f0-9]{8}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{12})',
 
- // This is the new padded version: exactly 22 characters long, padded with trailing '-'.
- short_uuid_regex: ':shortuuid([123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ-]{22})',
- deCamelCase,
- unshortenUuid
+  // A 22 character long shortened UUID, of the form [22]
+  short_uuid_regex: ':shortuuid([123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ-]{22})',
 }
-
