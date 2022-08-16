db642cdbf787e878fc6c6d9c95929faaf02b9615
Krishanu Majumdar <majumdar@liverpool.ac.uk>
2022-08-16 10:50:55 +0100

    Relaxing short UUID regex from exactly 22 characters to between 20 and 22

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -2,6 +2,6 @@ module.exports = {
   // A 36 character long full UUID, of the form: [8]-[4]-[4]-[4]-[12]
   uuid_regex: ':uuid([A-Fa-f0-9]{8}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{12})',
 
-  // A 22 character long shortened UUID, of the form [22]
-  short_uuid_regex: ':shortuuid([123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ-]{22})',
+  // A 20 to 22 character long shortened UUID
+  short_uuid_regex: ':shortuuid([123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ-]{20,22})',
 }
