afc9c8bcc9a4a9fc82264e7825f88e6f304c074a
Micah Henning <mhenning@fnal.gov>
2022-12-08 12:12:53 -0600

    copy node modules directory instead of using symbollic link. this enables npm-based vs code functionality like linting and typsecript

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
-ln -snfv /build/node_modules /app/node_modules
-npm start
+set -e
+
+# Ensure node_modules are found
+cp /build/package-lock.json /app/
+if [ ! -d /app/node_modules ]; then
+  echo "Copying node_modules from build directory to app directory..."
+  cp -a /build/node_modules /app
+fi
+
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
