34dd22664c782547e14e4137daab340059cddab2
Krishanu Majumdar <majumdar@liverpool.ac.uk>
2023-07-12 11:12:03 +0100

    Additional changes relating to tension measurements ... - reworked M2M tension measurements upload script so that all user-defined information is now at the very top - there should be no need for users to change anything outside this section - added ability to upload images to tension measurements action information pages - will be useful for including plots and such

--- m2m/template_upload_tensions.py
+++ m2m/template_upload_tensions.py
@@ -1,6 +1,35 @@
 from common import ConnectToAPI, PerformAction, EditAction
 from tensions import ExtractTensions
 
+####################################
+# Set the user-defined information #
+# ##################################
+
+# Set a flag to specify if you are performing a new tension measurements action, or editing an existing one with re-tensioning measurements
+new_tensionMeasurements = False
+
+# For extracting the tension measurements (required for BOTH performing new and editing existing actions), the following information is required:
+csvFile         = '~/Desktop/tensions/APA4_U_layer_v2.csv'
+                                                            # Name and location of the input .csv file (must be a string ending in '.csv')
+apaLayer        = 'U'                                       # Wire layer (must be given as one of 'X','U', 'V' or 'G')
+
+# For uploading new tension measurements (i.e. performing a new action), the following information is required:
+apa_uuid        = '49dcac80-4645-11ed-bb7f-0f80f77f8437'    # UUID of the Assembled APA on which the action is being performed (get from DB)
+winder_number   = 1                                         # Number
+winder_head     = '1'                                       # Free-form string
+measurement_sys = 'laser1'                                  # Use one of the following: 'dwa1', 'dwa2, 'dwa3', 'laser1', 'laser2', 'laser3', 'laser4', 'laser5'
+newAction_comms = 'Some comment about the new tension measurements, if needed'
+                                                            # Free-form string, additional description or commentary if required
+
+# For uploading re-tensioning measurements (i.e. editing an existing action), the following information is required:
+action_id       = '64ae78fd79d1db9823789440'                # ID of the existing tension measurements action to be edited (get from DB)
+replaced_wires  = 'Some description of the replaced wires or wire segments, if applicable'
+                                                            # Free-form string, additional description or commentary if required
+edtAction_comms = 'Some comment about the re-tensioning measurements, if needed'
+                                                            # Free-form string, additional description or commentary if required
+
+# ##################################
+
 
 # Main script function
 if __name__ == '__main__':
@@ -11,15 +40,9 @@ if __name__ == '__main__':
     ########################################
     # User-defined script functionality goes here
 
-    # Uploading tension measurements requires two steps:
+    # Uploading tension measurements (both new and re-tensionings) requires two steps:
     #   - extract the tension measurements from the input .csv file
     #   - perform a new tension measurements action, or edit an existing one
-
-    # Set the two arguments required to extract the tension measurements:
-    #   - the name of the input .csv file (must be a string ending in '.csv')
-    #   - the wire layer (must be given as one of 'X','U', 'V' or 'G')
-    csvFile = '~/Desktop/tensions/APA4_U_layer.csv'
-    apaLayer = 'U'
 
     # Call the extraction function ... this returns two Python lists - one for the tensions on side A, and the other for side B
     tensions_sideA, tensions_sideB = ExtractTensions(csvFile, apaLayer)
@@ -28,8 +51,42 @@ if __name__ == '__main__':
 
     # EITHER perform a new tension measurements action with 'tensions_sideA' and 'tensions_sideB' as part of the 'actionData' dictionary ...
     # OR edit an existing tension measurements action with 'tensions_sideA' and 'tensions_sideB' as part of the 'actionData_values' list of field values to be edited
-    # For more details about these options, please see the README, as well as the 'template_perform_action.py' and 'template_edit_action.py' scripts respectively
+    # For more general details about these options, please see the README and the 'template_perform_action.py' and 'template_edit_action.py' scripts respectively
+    if new_tensionMeasurements:
+        actionTypeFormID = 'x_tension_testing'
+        componentUUID = apa_uuid
+        actionData = {
+            'apaLayer': apaLayer.lower(),
+            'location': 'daresbury',
+            'winderNumber': winder_number,
+            'winderHead': winder_head,
+            'measurementSystem': measurement_sys,
+            'measuredTensions_sideA': tensions_sideA,
+            'measuredTensions_sideB': tensions_sideB,
+            'replacedWireSegs': '',
+            'comments': newAction_comms,
+        }
 
+        id = PerformAction(actionTypeFormID, componentUUID, actionData, connection, headers)
+        print(f" Successfully performed action with ID: {id}")
+    else:
+        actionID = action_id
+        actionData_fields = [
+            'measuredTensions_sideA',
+            'measuredTensions_sideB',
+            'replacedWireSegs',
+            'comments',
+        ]
+        actionData_values = [
+            tensions_sideA,
+            tensions_sideB,
+            replaced_wires,
+            edtAction_comms,
+        ]
+
+        id = EditAction(actionID, actionData_fields, actionData_values, connection, headers)
+        print(f" Successfully edited action with ID: {id}")
+    
     ########################################
 
     print()
