32441742b993698d40506fdff5cbc1d8fb235972
Krishanu Majumdar <majumdar@liverpool.ac.uk>
2024-05-29 18:46:14 +0100

    New centralised dictionaries of personnel ... - added centralised dictionaries for various groups of UK and US personnel, consisting of 'api name' and 'display name' pairs - exposed personnel dictionaries along JSON routes, for use as data sources in Formio 'Select' type form components

--- app/routes/api/users.js
+++ app/routes/api/users.js
@@ -4,6 +4,7 @@ const router = require('express').Router();
 const { AUTH0_DOMAIN, AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET } = require('../../lib/constants');
 const logger = require('../../lib/logger');
 const permissions = require('../../lib/permissions');
+const utils = require('../../lib/utils');
 
 /// To ensure the manager below works:
 ///    - go to Auth0 Dashboard -> Applications -> APIs -> Auth0ManagementAPI
@@ -56,5 +57,114 @@ router.get('/users/list', permissions.checkPermissionJson('users:view'), async f
   }
 });
 
+
+/// List all UK and US technicians
+router.get('/technicians.json', async function (req, res, next) {
+  try {
+    // Convert the centralised list of technicians from a dictionary to an array, with each element of the array being a sub-dictionary containing the API and display names of each person
+    let data = [];
+
+    for (const person in utils.dictionary_technicians) {
+      data.push({
+        api_name: person,
+        display_name: utils.dictionary_technicians[person],
+      });
+    }
+
+    // Return the array in JSON format
+    return res.json(data);
+  } catch (err) {
+    logger.info({ route: req.route.path }, err.message);
+    res.status(500).json({ error: err.toString() });
+  }
+});
+
+
+/// List all lead personnel at the UK and US APA factories
+router.get('/apaFactoryLeads.json', async function (req, res, next) {
+  try {
+    // Convert the centralised list of lead personnel from a dictionary to an array, with each element of the array being a sub-dictionary containing the API and display names of each person
+    let data = [];
+
+    for (const person in utils.dictionary_apaFactoryLeads) {
+      data.push({
+        api_name: person,
+        display_name: utils.dictionary_apaFactoryLeads[person],
+      });
+    }
+
+    // Return the array in JSON format
+    return res.json(data);
+  } catch (err) {
+    logger.info({ route: req.route.path }, err.message);
+    res.status(500).json({ error: err.toString() });
+  }
+});
+
+
+/// List all personnel who are authorised to sign-off on tension controls
+router.get('/tensionControlSignoff.json', async function (req, res, next) {
+  try {
+    // Convert the centralised list of personnel from a dictionary to an array, with each element of the array being a sub-dictionary containing the API and display names of each person
+    let data = [];
+
+    for (const person in utils.dictionary_tensionControlSignoff) {
+      data.push({
+        api_name: person,
+        display_name: utils.dictionary_tensionControlSignoff[person],
+      });
+    }
+
+    // Return the array in JSON format
+    return res.json(data);
+  } catch (err) {
+    logger.info({ route: req.route.path }, err.message);
+    res.status(500).json({ error: err.toString() });
+  }
+});
+
+
+/// List all personnel who are authorised to sign-off on winder maintenance
+router.get('/winderMaintenanceSignoff.json', async function (req, res, next) {
+  try {
+    // Convert the centralised list of personnel from a dictionary to an array, with each element of the array being a sub-dictionary containing the API and display names of each person
+    let data = [];
+
+    for (const person in utils.dictionary_winderMaintenanceSignoff) {
+      data.push({
+        api_name: person,
+        display_name: utils.dictionary_winderMaintenanceSignoff[person],
+      });
+    }
+
+    // Return the array in JSON format
+    return res.json(data);
+  } catch (err) {
+    logger.info({ route: req.route.path }, err.message);
+    res.status(500).json({ error: err.toString() });
+  }
+});
+
+
+/// List all personnel who are authorised to sign-off on APA frame and grounding mesh intakes
+router.get('/frameMeshSignoff.json', async function (req, res, next) {
+  try {
+    // Convert the centralised list of personnel from a dictionary to an array, with each element of the array being a sub-dictionary containing the API and display names of each person
+    let data = [];
+
+    for (const person in utils.dictionary_frameMeshSignoff) {
+      data.push({
+        api_name: person,
+        display_name: utils.dictionary_frameMeshSignoff[person],
+      });
+    }
+
+    // Return the array in JSON format
+    return res.json(data);
+  } catch (err) {
+    logger.info({ route: req.route.path }, err.message);
+    res.status(500).json({ error: err.toString() });
+  }
+});
 
 module.exports = router;
