191e6a7056a183fcb623a247c60f1f2598c54e1e
Krishanu Majumdar <majumdar@liverpool.ac.uk>
2024-08-09 11:59:45 +0100

    Additional changes to API workflow route ... - removed limit on the number of workflows that can be retrieved through the 'list of workflows of a single type' route - this route now returns a list of workflow statuses as well as the original list of workflow IDs - corresponding change in the M2M common functions, to account for the new nested list return from the above route (this is the only place where the route is used)

--- app/routes/api/workflows.js
+++ app/routes/api/workflows.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
+const Actions = require('../../lib/Actions');
 const logger = require('../../lib/logger');
 const permissions = require('../../lib/permissions');
 const Workflows = require('../../lib/Workflows');
@@ -49,17 +50,34 @@ router.get('/workflows/:typeFormId/list', permissions.checkPermissionJson('workf
   try {
     // Retrieve records of all workflows with the specified workflow type
     // The first argument should be an object consisting of the match condition, i.e. the type form ID to match to
-    const workflows = await Workflows.list({ typeFormId: req.params.typeFormId }, { limit: 500 });
+    const workflows = await Workflows.list({ typeFormId: req.params.typeFormId });
 
-    // Extract only the ID field (in string format) from each workflow record, and save it into a list to be returned
+    // Extract the ID field (in string format) from each workflow record, and save it into a list
+    // Additionally, determine if each workflow is complete or in progress, and save these statuses into a separate list
     let workflowIDs = [];
+    let workflowStatuses = [];
 
     for (const workflow of workflows) {
       workflowIDs.push(workflow.workflowId);
+
+      let numberOfCompleteActions = 0;
+
+      for (let stepIndex = 1; stepIndex < workflow.stepResultIDs.length; stepIndex++) {
+        if (workflow.stepResultIDs[stepIndex].length > 0) {
+          const action = await Actions.retrieve(workflow.stepResultIDs[stepIndex]);
+
+          if (action.data.actionComplete) {
+            numberOfCompleteActions++;
+          }
+        }
+      }
+
+      const workflowStatus = (numberOfCompleteActions === workflow.stepResultIDs.length - 1) ? 'Complete' : 'In Progress';
+      workflowStatuses.push(workflowStatus);
     }
 
-    // Return the list of workflow IDs
-    return res.json(workflowIDs);
+    // Return a list containing both the list of workflow IDs and the list of workflow statuses
+    return res.json([workflowIDs, workflowStatuses]);
   } catch (err) {
     logger.info({ route: req.route.path }, err.message);
     res.status(500).json({ error: err.toString() });
