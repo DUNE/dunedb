80d62b977e20c4ea7a84d314b3c91d17c4f71e3b
Krishanu Majumdar <majumdar@liverpool.ac.uk>
2025-08-01 13:59:18 +0100

    Adding ASF component naming and DUNE PID construction ... - new ASF components will be given names of the format 'ASF 00001-UK', etc. with the user providing the typeRecordNumber and location at component creation - DUNE PIDs are not assigned, but a template is still set to be consistent with other component types - added similar code to the admin utility for renaming components (for completeness ... there are no ASF components currently in the DB)

--- app/lib/Components.js
+++ app/lib/Components.js
@@ -101,6 +101,21 @@ async function save(input, req) {
       }
 
       newRecord.data.dunePid = `D00300200001-${typeRecordNumber}-${pidSuffix}`;
+    } else if (newRecord.formId === 'APAShippingFrame') {
+      let pidSuffix = '';
+
+      if (newRecord.data.asfLocation === 'chicago') {
+        newRecord.data.componentName = `ASF ${typeRecordNumber}-US`;
+        pidSuffix = 'US175-010000';
+      } else if (newRecord.data.asfLocation === 'daresbury') {
+        newRecord.data.componentName = `ASF ${typeRecordNumber}-UK`;
+        pidSuffix = 'UK106-010000';
+      } else if (newRecord.data.asfLocation === 'wisconsin') {
+        newRecord.data.componentName = `ASF ${typeRecordNumber}-US`;
+        pidSuffix = 'US200-010000';
+      }
+
+      newRecord.data.dunePid = `D003MMMNNNNN-${typeRecordNumber}-${pidSuffix}`;
     } else if (newRecord.formId === 'AssembledAPA') {
       let pidPrefix = '';
       let pidSuffix = '';
