de69b2878f36d79418bc71cdf828f45bd2da7789
Krishanu Majumdar <majumdar@liverpool.ac.uk>
2026-08-12 16:10:59 +0100

    Cleaning up type records (part 2) ... - removing file and references relating to the common schema (validity and insertion objects), which are now no longer used - set admin utility to remove validity and insertion objects from all records in a specified collection

--- app/lib/Actions.js
+++ app/lib/Actions.js
@@ -1,7 +1,6 @@
 const MUUID = require('uuid-mongodb');
 const ObjectId = require('mongodb').ObjectId;
 
-const commonSchema = require('./commonSchema');
 const Components = require('./Components');
 const { db } = require('./db');
 const dbLock = require('./dbLock');
